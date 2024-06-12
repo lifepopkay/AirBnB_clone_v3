1df27513392ef00a3d404dfe438564128ea29dc9
lifepopkay <popoolafolas@gmail.com>
2024-06-13 00:47:05 +0100

    task

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ Script that imports a Blueprint and runs Flask """
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -9,7 +8,6 @@ from os import getenv
 app = Flask(__name__)
 
 app.register_blueprint(app_views)
-CORS(app,  resources={r"/*": {"origins": "0.0.0.0"}})
 
 host = getenv("HBNB_API_HOST", "0.0.0.0")
 port = getenv("HBNB_API_PORT", "5000")
