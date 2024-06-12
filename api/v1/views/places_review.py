6c0750fd7207094587b882c2d12e7d0e10e6fcc0
onlyBel <122991022+onlyBel@users.noreply.github.com>
2024-06-12 05:45:06 +0300

    Update places_review.py

--- api/v1/views/places_review.py
+++ api/v1/views/places_review.py
@@ -62,43 +62,41 @@ def view_reviews_of_place(id):
                         # Save the object in storage
                         storage.new(obj)
                         storage.save()
-												return jsonify(obj.to_dict()), 201
+			      return jsonify(obj.to_dict()), 201
 												
-												
-												@app_views.route('/review/<id>',
+													@app_views.route('/review/<id>',
 																				 strict_slashes=False,
 																				 methods=['GET', 'DELETE', 'PUT'])
 												def view_review_id(id):
-                                                                                                                                                                                                                                                                                                                                            """Returns or erases a city"""
-                                                                                                                                                                                                                                                                                                                                                        review_obj = storage.get(Review, id)
+													"""Returns or erases a city"""
+													review_obj = storage.get(Review, id)
+													
+													if review_obj is None:
+														return abort(404)
 
-                                                                                                                                                                                                                                                                                                                                                            if review_obj is None:
-                                                                                                                                                                                                                                                                                                                                                                        return abort(404)
+                          if request.method == 'GET':
+                            return jsonify(review_obj.to_dict())
 
-                                                                                                                                                                                                                                                                                                                                                                        if request.method == 'GET':
-                                                                                                                                                                                                                                                                                                                                                                                    return jsonify(review_obj.to_dict())
+                          if request.method == 'DELETE':
+														storage.delete(review_obj)
+														storage.save()
+														return jsonify({}), 200
 
-                                                                                                                                                                                                                                                                                                                                                                                    if request.method == 'DELETE':
-                                                                                                                                                                                                                                                                                                                                                                                                storage.delete(review_obj)
-                                                                                                                                                                                                                                                                                                                                                                                                        storage.save()
-                                                                                                                                                                                                                                                                                                                                                                                                                return jsonify({}), 200
-
-                                                                                                                                                                                                                                                                                                                                                                                                                if request.method == 'PUT':
-                                                                                                                                                                                                                                                                                                                                                                                                                            data = request.get_json()
-                                                                                                                                                                                                                                                                                                                                                                                                                                    if isinstance(data, dict):
-                                                                                                                                                                                                                                                                                                                                                                                                                                                    pass
-                                                                                                                                                                                                                                                                                                                                                                                                                                                        else:
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        return jsonify({"error": "Not a JSON"}), 400
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                            if 'id' in data.keys():
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            data.pop("id")
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    if 'created_at' in data.keys():
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    data.pop("created_at")
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            if 'updated_at' in data.keys():
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            data.pop("updated_at")
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    for key, value in data.items():
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    setattr(review_obj, key, value)
-
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            storage.save()
+                          if request.method == 'PUT':
+														data = request.get_json()
+                          if isinstance(data, dict):
+														pass
+													else:
+														return jsonify({"error": "Not a JSON"}), 400
+														
+														if 'id' in data.keys():
+															data.pop("id")
+															if 'created_at' in data.keys():
+																data.pop("created_at")
+																if 'updated_at' in data.keys():
+																	data.pop("updated_at")
+																	
+																	for key, value in data.items():
+																		setattr(review_obj, key, value
+																						storage.save()
                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     return jsonify(review_obj.to_dict())
